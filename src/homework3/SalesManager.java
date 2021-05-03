5a178a468960c4c3f9f45468f7a863ad76073580
== <==>
2021-05-03 14:52:05 +0300

    ikinci commit

--- src/homework3/SalesManager.java
+++ src/homework3/SalesManager.java
@@ -1,5 +1,26 @@
 package homework3;
 
-public class SalesManager {
+public class SalesManager implements SalesService{
+
+	@Override
+	public void add(Sales sales, Game game, Customer customer,Campaign campaign) {
+		System.out.println(sales.getId()+"'li sipariþe ait olan "+game.getProductName()+" oyunu"+
+				customer.getFirstName()+" "+customer.getLastName()+" müþterimize"+campaign.getDiscountRate() + "indirim oranýyla satýldý");
+		
+	}
+
+	@Override
+	public void delete(Sales sales) {
+		System.out.println(sales.getId()+" nolu sipariþ silinmiþtir.");
+		
+	}
+
+	@Override
+	public void update(Sales sales) {
+		System.out.println(sales.getId()+" nolu sipariþ güncellenmiþtir.");
+
+		
+	}
+
 
 }
--- src/homework3/SalesService.java
+++ src/homework3/SalesService.java
@@ -1,5 +1,7 @@
 package homework3;
 
 public interface SalesService {
-	
+	void add(Sales sales,Game game,Customer customer,Campaign campaign);
+	void delete(Sales sales);
+	void update(Sales sales);
 }
